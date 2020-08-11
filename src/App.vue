36a8b46c41a1eb5cfea9722fd6c6e94b2f1886c2
Oscar Grainger <ogr@ehtrace.com>
2020-08-12 01:11:20 +0200

    weather

--- src/App.vue
+++ src/App.vue
@@ -17,11 +17,30 @@
       <v-container>
         <v-row>
           <v-col cols="12">
+
             <place-search v-model="coordinates" />
           </v-col>
+          <v-col cols="12">
+            <v-card>
+              <v-row>
+                <v-col md="10">
+                  <span class="text-h4" v-text="`${weatherInfos && weatherInfos.name ? weatherInfos.name : 'No city found'}${weatherInfos && weatherInfos.sys.country ? ` - ${weatherInfos.sys.country}` : ''}`" />
+                </v-col>
+                <v-col md="2">
+                  <v-img class="ma-n4" max-width="10vh" v-if="weatherIcon" :src="`http://openweathermap.org/img/wn/${weatherIcon}@2x.png`" />
+                  {{currentTemperatureString}}
+                </v-col>
+              </v-row>
+
+            </v-card>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          {{ azimuthInfos }}
         </v-row>
         <v-row>
-          {{ place }}
+          {{ weatherInfos }}
         </v-row>
       </v-container>
     </v-main>
@@ -31,7 +50,15 @@
 <script>
 import PlaceSearch from '@/components/PlaceSearch.vue';
 
-const API_KEY = 'f2b5fcdad58f47ebb86d4d4c93178d14'
+const IP_GEOLOCATION_API_KEY = 'f2b5fcdad58f47ebb86d4d4c93178d14'
+const OPEN_WEATHER_API_KEY = '84744bcb3ccf3dc67cf8426f62bb395c'
+
+const TempUnit = {
+  C:'C',
+  K: 'K',
+  F: 'F'
+
+}
 
 export default {
   name: 'App',
@@ -42,20 +69,48 @@ export default {
 
   data: () => ({
     coordinates: {},
-    place: {}
+    azimuthInfos: null,
+    weatherInfos: null,
+    units: {
+      temperature: TempUnit.C
+    }
+
   }),
+  computed: {
+    weatherIcon() {
+      return this.weatherInfos?.weather[0]?.icon
+    },
+    currentTemperatureString() {
+      return this[this.units.temperature](this.weatherInfos?.main.temp)
+    }
+  },
   methods: {
-    getPlaceInfos() {
-      fetch(`https://api.ipgeolocation.io/astronomy?apiKey=${API_KEY}&lat=${this.coordinates.lat}&long=${this.coordinates.lng}`)
+    getAzimuthInfos() {
+      fetch(`https://api.ipgeolocation.io/astronomy?apiKey=${IP_GEOLOCATION_API_KEY}&lat=${this.coordinates.lat}&long=${this.coordinates.lng}`)
         .then(r => r.json())
-        .then(r => this.place = r)
+        .then(r => this.azimuthInfos = r)
+    },
+    getWeatherInfos() {
+      fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${this.coordinates.lat}&lon=${this.coordinates.lng}&appid=${OPEN_WEATHER_API_KEY}`)
+        .then(r => r.json())
+        .then(r => this.weatherInfos = r)
+    },
+    K(t) {
+      return `${Math.round(t)}°K`
+    },
+    C(t) {
+      return `${Math.round(t - 273.15)}°C`
+    },
+    F(t) {
+      return `${(Math.round(t - 273.15) * 1.8 + 32)}°F`
     },
   },
   watch: {
     coordinates(val) {
       this.coordinates = val
       localStorage.setItem('coordinates', JSON.stringify(val))
-      this.getPlaceInfos()
+      this.getAzimuthInfos()
+      this.getWeatherInfos()
     }
   },
   mounted() {
