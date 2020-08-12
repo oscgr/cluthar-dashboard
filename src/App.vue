f362e8605b131965f5fa23a3117a8c9f476e46be
Oscar Grainger <ogr@ehtrace.com>
2020-08-13 00:23:35 +0200

    weather card

--- src/App.vue
+++ src/App.vue
@@ -17,30 +17,11 @@
       <v-container>
         <v-row>
           <v-col cols="12">
-
             <place-search v-model="coordinates" />
           </v-col>
-          <v-col cols="12">
-            <v-card>
-              <v-row>
-                <v-col md="10">
-                  <span class="text-h4" v-text="`${weatherInfos && weatherInfos.name ? weatherInfos.name : 'No city found'}${weatherInfos && weatherInfos.sys.country ? ` - ${weatherInfos.sys.country}` : ''}`" />
-                </v-col>
-                <v-col md="2">
-                  <v-img class="ma-n4" max-width="10vh" v-if="weatherIcon" :src="`http://openweathermap.org/img/wn/${weatherIcon}@2x.png`" />
-                  {{currentTemperatureString}}
-                </v-col>
-              </v-row>
-
-            </v-card>
+          <v-col sm="6" md="4">
+            <weather-card :weather-infos="weatherInfos" />
           </v-col>
-        </v-row>
-
-        <v-row>
-          {{ azimuthInfos }}
-        </v-row>
-        <v-row>
-          {{ weatherInfos }}
         </v-row>
       </v-container>
     </v-main>
@@ -49,18 +30,13 @@
 
 <script>
 import PlaceSearch from '@/components/PlaceSearch.vue';
-
-const TempUnit = {
-  C:'C',
-  K: 'K',
-  F: 'F'
-
-}
+import WeatherCard from "@/components/WeatherCard";
 
 export default {
   name: 'App',
 
   components: {
+    WeatherCard,
     PlaceSearch,
   },
 
@@ -68,19 +44,7 @@ export default {
     coordinates: {},
     azimuthInfos: null,
     weatherInfos: null,
-    units: {
-      temperature: TempUnit.C
-    }
-
   }),
-  computed: {
-    weatherIcon() {
-      return this.weatherInfos?.weather[0]?.icon
-    },
-    currentTemperatureString() {
-      return this[this.units.temperature](this.weatherInfos?.main.temp)
-    }
-  },
   methods: {
     getAzimuthInfos() {
       fetch(`https://api.ipgeolocation.io/astronomy?apiKey=${process.env.VUE_APP_IP_GEOLOCATION_API_KEY}&lat=${this.coordinates.lat}&long=${this.coordinates.lng}`)
@@ -88,18 +52,9 @@ export default {
         .then(r => this.azimuthInfos = r)
     },
     getWeatherInfos() {
-      fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${this.coordinates.lat}&lon=${this.coordinates.lng}&appid=${process.env.VUE_APP_OPEN_WEATHER_API_KEY}`)
+      fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${this.coordinates.lat}&lon=${this.coordinates.lng}&units=metric&appid=${process.env.VUE_APP_OPEN_WEATHER_API_KEY}`)
         .then(r => r.json())
         .then(r => this.weatherInfos = r)
-    },
-    K(t) {
-      return `${Math.round(t)}°K`
-    },
-    C(t) {
-      return `${Math.round(t - 273.15)}°C`
-    },
-    F(t) {
-      return `${(Math.round(t - 273.15) * 1.8 + 32)}°F`
     },
   },
   watch: {
---
+++ src/components/WeatherCard.vue
@@ -0,0 +1,91 @@
+<template>
+  <v-card>
+    <v-card-text class="py-0">
+      <v-row>
+        <v-col cols="8">
+          <span class="text-h4" v-text="temperature" />
+          <v-spacer />
+          <span class="text-h5" v-text="weather" />
+          <v-spacer />
+        </v-col>
+        <v-col cols="4" class="pr-0">
+          <v-img class="my-n3 mr-n3" v-if="weatherIcon" :src="`http://openweathermap.org/img/wn/${weatherIcon}@2x.png`" />
+        </v-col>
+        <v-col cols="6" class="pt-0">
+          <v-icon>mdi mdi-cloud-outline</v-icon>
+          <span class="text-h6" v-text="clouds" />
+          <v-spacer />
+          <v-icon>mdi mdi-weather-windy</v-icon>
+          <span class="text-h6" v-text="wind" />
+        </v-col>
+        <v-col cols="6" class="pt-0 text-right">
+          <v-icon>mdi mdi-water-percent</v-icon>
+          <span class="text-h6" v-text="humidity" />
+          <v-spacer />
+          <v-icon>mdi mdi-gauge</v-icon>
+          <span class="text-h6" v-text="pressure" />
+        </v-col>
+      </v-row>
+      <v-row v-if="weatherInfos && weatherInfos.rain">
+        <v-col cols="6" class="pt-0">
+          <v-icon>mdi mdi-weather-pouring</v-icon>
+          <span class="text-h6" v-text="cumulation('rain', '1h')" />
+        </v-col>
+        <v-col cols="6" class="pt-0 text-right">
+          <v-icon>mdi mdi-weather-pouring</v-icon>
+          <span class="text-h6" v-text="cumulation('rain', '3h')" />
+        </v-col>
+      </v-row>
+      <v-row v-if="weatherInfos && weatherInfos.snow">
+        <v-col cols="6" class="pt-0">
+          <v-icon>mdi mdi-snowflake</v-icon>
+          <span class="text-h6" v-text="cumulation('snow', '1h')" />
+        </v-col>
+        <v-col cols="6" class="pt-0 text-right">
+          <v-icon>mdi mdi-snowflake</v-icon>
+          <span class="text-h6" v-text="cumulation('snow', '3h')" />
+        </v-col>
+      </v-row>
+    </v-card-text>
+  </v-card>
+</template>
+<script>
+
+export default {
+  name: 'WeatherCard',
+  props: {
+    weatherInfos: {},
+  },
+  data: () => ({
+    imageUrl: null,
+  }),
+  computed: {
+    weatherIcon() {
+      return this.weatherInfos?.weather[0]?.icon
+    },
+    temperature() {
+      return `${this.weatherInfos?.main.temp}°C`
+    },
+    clouds() {
+      return this.weatherInfos?.clouds?.all ? `${this.weatherInfos?.clouds?.all}%` : 'unknown'
+    },
+    wind() {
+      return (this.weatherInfos?.wind?.speed ? Math.round(this.weatherInfos?.wind?.speed * 3.6) : '?') + ' km/h'
+    },
+    weather() {
+      return this.weatherInfos?.weather[0]?.description
+    },
+    pressure() {
+      return this.weatherInfos?.main.pressure + ' hPa'
+    },
+    humidity() {
+      return this.weatherInfos?.main.humidity + '%'
+    },
+  },
+  methods: {
+    cumulation(type, duration) {
+      return (this.weatherInfos[type][duration] ? this.weatherInfos[type][duration] : '?') + ` mm (${duration})`
+    },
+  }
+}
+</script>
