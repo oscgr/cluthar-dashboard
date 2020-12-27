0bbd43e3efa5bf834a3faf5584959b2a57bc2cf8
Oscar Grainger <ogr@ehtrace.com>
2020-12-27 11:22:29 +0100

    better place card

--- src/components/PlaceCard.vue
+++ src/components/PlaceCard.vue
@@ -1,10 +1,4 @@
 <template>
-<!--    <v-card-->
-<!--      elevation="0"-->
-<!--      width="100%"-->
-<!--      height="100%"-->
-<!--      @click=""-->
-<!--    >-->
       <v-card
         elevation="0"
         width="100%"
@@ -12,18 +6,15 @@
       >
       <v-card-text>
         <v-row no-gutters class="display-3 mb-2">
-          <v-col cols="9">
+          <v-col cols="12">
             <span v-if="coordinates.lat" v-text="placeName ? placeName : 'Sans nom'" />
             <span v-else v-text="'Choisissez un endroit...'" />
-          </v-col>
-          <v-col cols="3" class="text-right" >
-            <v-icon v-if="coordinates.lat" size="50" title="endroit" v-text="mdiMapMarker" />
-            <v-icon v-else size="50" title="rechercher" v-text="mdiHomeSearchOutline" />
           </v-col>
         </v-row>
         <v-row no-gutters class="display-1">
           <v-col cols="6" class="">
-            <span v-text="dateString" />
+            <span class="hidden-md-and-down" v-text="dateString" />
+            <span class="hidden-lg-and-up" v-text="dateStringShort" />
           </v-col>
           <v-col cols="6" class="text-right">
             <span v-text="timeString" />
@@ -55,7 +46,7 @@ import {computed} from "@vue/composition-api";
 import coordinatesStore from "@/store/coordinatesStore";
 import weatherStore from "@/store/weatherStore";
 
-import {mdiMapMarker, mdiHomeSearchOutline, mdiLatitude, mdiLongitude, mdiEarth, mdiMapClock, mdiCrosshairs, mdiCrosshairsGps} from '@mdi/js'
+import {mdiLatitude, mdiLongitude, mdiEarth, mdiMapClock, mdiCrosshairs, mdiCrosshairsGps} from '@mdi/js'
 import timeStore from "@/store/timeStore";
 
 export default {
@@ -67,19 +58,21 @@ export default {
 
 
     const dateString = computed(() => now.value.format('dddd LL'))
+    const dateStringShort = computed(() => now.value.format('ddd ll'))
     const timeString = computed(() => now.value.format('LTS'))
 
     return {
       geolocationLoading,
       setGeolocationCoordinates,
       dateString,
+      dateStringShort,
       timeString,
       coordinates,
       placeName,
       placeCountry,
       timezone,
       // icons
-      mdiMapMarker, mdiHomeSearchOutline, mdiLatitude, mdiLongitude, mdiEarth, mdiMapClock, mdiCrosshairs, mdiCrosshairsGps
+      mdiLatitude, mdiLongitude, mdiEarth, mdiMapClock, mdiCrosshairs, mdiCrosshairsGps
     }
   }
 }
