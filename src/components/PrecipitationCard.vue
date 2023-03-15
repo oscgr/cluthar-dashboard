9d0b41dfcd24bbaa7c3fd26c18e205832750e875
Oscar Grainger <ogr@ehtrace.com>
2023-03-15 19:39:35 +0100

    precipitation subtext

--- src/components/PrecipitationCard.vue
+++ src/components/PrecipitationCard.vue
@@ -5,6 +5,7 @@
     flat
   >
     <v-card-title v-text="`Précipitation dans l'heure`" />
+    <v-card-subtitle v-text="subtext" />
     <v-card-text v-if="noData">
       <span class="text-h5" v-text="'Pas d\'informations'" />
     </v-card-text>
@@ -14,20 +15,13 @@
 
     <VueApexCharts
       v-else
+      :key="dark"
       style="pointer-events: none"
       type="heatmap"
       :series="series"
       :options="chartOptions"
-      height="150"
+      height="120"
     />
-    <!--      <v-sparkline -->
-    <!--        v-if="anyPrecipitation" -->
-    <!--        fill -->
-    <!--        padding="0" -->
-    <!--        :color="$vuetify.theme.dark ? 'white': 'primary'" -->
-    <!--        smooth -->
-    <!--        :value="precipitation" -->
-    <!--      /> -->
   </v-card>
 </template>
 
@@ -37,56 +31,93 @@ import { computed } from 'vue'
 import type { ApexOptions } from 'apexcharts'
 import { chunk, floor } from 'lodash'
 import { DateTime } from 'luxon'
+import { useI18n } from 'vue-i18n'
+import { useDark } from '@vueuse/core'
 import useWeather from '@/store/weather'
 import Global from '@/utils/global'
+import PrecipitationLevel from '@/enums/PrecipitationLevel'
+import { diffNowLocaleString } from '@/utils/durationDisplay'
 
 const CHART_CHUNK_SIZE = 10
 
-const { loading, payload } = useWeather()
+const { t } = useI18n()
+const { payload } = useWeather()
 
+const dark = useDark()
 const noData = computed<boolean>(() => typeof payload.value.minutely === 'undefined')
 const anyPrecipitation = computed<boolean>(() => payload.value.minutely?.some(m => m.precipitation > 0) || false)
 
 const chunkedMinutely = computed(() => chunk(payload.value.minutely || [], CHART_CHUNK_SIZE).map(([first]) => first))
 
+const getPrecipitationLevel = (precipitation: number) => {
+  switch (true) {
+    case precipitation < 0.1:
+      return PrecipitationLevel.NONE
+    case precipitation < 1:
+      return PrecipitationLevel.DROPS
+    case precipitation < 3:
+      return PrecipitationLevel.LIGHT
+    case precipitation < 7:
+      return PrecipitationLevel.MODERATE
+    case precipitation >= 7:
+      return PrecipitationLevel.HEAVY
+    default:
+      return PrecipitationLevel.NONE
+  }
+}
+
+const subtext = computed(() => {
+  if (typeof chunkedMinutely.value[0]?.precipitation === 'undefined')
+    return ''
+  if (chunkedMinutely.value[0]?.precipitation !== 0)
+    return t(`constants.PRECIPITATION_LEVEL.${getPrecipitationLevel(chunkedMinutely.value[0]?.precipitation)}`)
+  else if (anyPrecipitation.value)
+    return `${t(`constants.PRECIPITATION_LEVEL.${getPrecipitationLevel(chunkedMinutely.value.find(({ precipitation }) => precipitation > 0)?.precipitation)}`)} ${diffNowLocaleString(DateTime.fromMillis(chunkedMinutely.value.find(({ precipitation }) => precipitation > 0)?.dt * 1000))}`
+})
+
+/* ==================== CHART ==================== */
+
 const chartOptions = computed<ApexOptions>(() => {
   return {
     ...Global.getGlobalApexChartOptions(),
+    stroke: {
+      colors: ['transparent'],
+    },
     plotOptions: {
       heatmap: {
         radius: 0,
         enableShades: false,
-        useFillColorAsStroke: true,
         colorScale: {
           ranges: [{
             from: 0,
             to: 0.1,
-            color: '#FFFFFF',
-            foreColor: '#000000',
-            name: 'Nulle',
+            color: 'transparent',
+            // foreColor: '#000000',
+            name: PrecipitationLevel.NONE,
           }, {
             from: 0.1,
             to: 1,
-            color: '#cee3fd',
-            foreColor: '#000000',
-            name: 'Gouttes',
+            color: 'rgba(1,128,255,0.1)',
+            foreColor: dark.value ? '#EEEEEE' : '#000000',
+            name: PrecipitationLevel.DROPS,
           }, {
             from: 1,
             to: 3,
-            color: '#75b9ff',
-            // foreColor: undefined,
-            name: 'Pluie faible',
+            color: 'rgba(1,128,255,0.2)',
+            foreColor: dark.value ? '#EEEEEE' : '#000000',
+            name: PrecipitationLevel.LIGHT,
           }, {
             from: 3,
             to: 7,
-            color: '#007fff',
+            color: 'rgba(1,128,255,0.4)',
             // foreColor: undefined,
-            name: 'Pluie modérée',
+            name: PrecipitationLevel.MODERATE,
           }, {
             from: 7,
-            color: '#004386',
+            to: 10000,
+            color: 'rgba(1,128,255,0.7)',
             // foreColor: undefined,
-            name: 'Pluie forte',
+            name: PrecipitationLevel.HEAVY,
           }],
           inverse: false,
           min: 0,
@@ -115,27 +146,16 @@ const chartOptions = computed<ApexOptions>(() => {
       axisTicks: {
         show: false,
       },
-    //   // tickAmount: 5,
-    //   // categories: chunk(payload.value.minutely?.map(m => m.dt) || [], CHART_CHUNK_SIZE).map(([first]) => first),
     },
     yaxis: {
     //   min: 0,
       labels: {
         show: false,
-        // formatter: (value: number) => {
-        //   return `${Math.floor(value * 1000)} mm`
-        // },
       },
       axisBorder: {
         show: false,
       },
-    //   axisTicks: {
-    //     show: false,
-    //   },
-    //   tickAmount: 5,
-    //   // categories: chunk(payload.value.minutely?.map(m => m.dt) || [], CHART_CHUNK_SIZE).map(([first]) => first),
     },
-    // colors: ['rgba(20,135,197,1)'],
     dataLabels: {
       enabled: true,
       formatter(val) {
@@ -143,15 +163,11 @@ const chartOptions = computed<ApexOptions>(() => {
           return
         return `${floor(Number(val), 1)}mm`
       },
-      style: {
-        // colors: ['#fff'],
-      },
     },
   }
 },
 )
 const series = computed<{ name: string; data: { x: number; y: number } }[]>(() => {
-  console.table(chunkedMinutely.value)
   return [{
     name: 'rain',
     data: chunkedMinutely.value.map(({ dt, precipitation }) => ({ x: dt, y: precipitation })),
---
+++ src/enums/PrecipitationLevel.ts
@@ -0,0 +1,8 @@
+enum PrecipitationLevel {
+  NONE = 'NONE',
+  DROPS = 'DROPS',
+  LIGHT = 'LIGHT',
+  MODERATE = 'MODERATE',
+  HEAVY = 'HEAVY',
+}
+export default PrecipitationLevel
