70bd83abcb395bf07c90f825b06ac99b0576cb05
Oscar Grainger <ogr@ehtrace.com>
2023-03-26 22:35:36 +0200

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import path from 'node:path'
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
-import { VitePWA } from 'vite-plugin-pwa'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 
 export default defineConfig({
@@ -19,43 +18,6 @@ export default defineConfig({
 
       include: path.resolve(__dirname, './src/locales/**'),
     }),
-    // https://github.com/antfu/vite-plugin-pwa
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Cluthar dashboard',
-        short_name: 'Cluthar',
-        theme_color: '#A89D8D',
-        background_color: '#f0ece3',
-        icons: [
-          {
-            src: 'public/manifest-icon-192.maskable.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: 'any',
-          },
-          {
-            src: 'public/manifest-icon-192.maskable.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: 'maskable',
-          },
-          {
-            src: 'public/manifest-icon-512.maskable.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any',
-          },
-          {
-            src: 'public/manifest-icon-512.maskable.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable',
-          },
-        ],
-      },
-    }),
-
   ],
 
   optimizeDeps: {
