42b8292e1a6d08a5e9209fe1d80df0606c0034d1
ogr <ogr@ehtrace.com>
2023-05-21 07:54:51 +0200

    NASA potd toggle infos

--- src/components/Cards/NasaPictureOfTheDayCard.vue
+++ src/components/Cards/NasaPictureOfTheDayCard.vue
@@ -7,11 +7,12 @@
       dark
       flat
       :color="dark ? '#0b3c5d' : '#c8cdd0'"
+      @click="() => toggleDisplayInfos()"
     >
       <v-img
         v-if="payload.url"
         aspect-ratio="16/9"
-        :style="{ filter: isHovering ? 'opacity(0.2)' : '' }"
+        :style="{ filter: (isHovering || displayInfos) ? 'opacity(0.2)' : '' }"
         style="max-height: 500px;"
         cover :src="payload.hdurl" :lazy-src="payload.url" :alt="`${payload.title} - Copyright ${payload.copyright}`"
       >
@@ -24,15 +25,15 @@
           </div>
         </template>
       </v-img>
-      <v-card-title v-show="isHovering" class=" position-absolute" style="top: 0" v-text="payload.title" />
-      <v-card-subtitle v-show="isHovering" class=" position-absolute" style="top: 40px" v-text="`© ${payload.copyright}`" />
-      <v-card-text v-show="isHovering" class=" position-absolute text-justify" style="top: 70px" v-text="payload.explanation" />
+      <v-card-title v-show="isHovering || displayInfos" class=" position-absolute" style="top: 0" v-text="payload.title" />
+      <v-card-subtitle v-show="isHovering || displayInfos" class=" position-absolute" style="top: 40px" v-text="`© ${payload.copyright}`" />
+      <v-card-text v-show="isHovering || displayInfos" class=" position-absolute text-justify" style="top: 70px" v-text="payload.explanation" />
     </v-card>
   </v-hover>
 </template>
 
 <script lang="ts" setup>
-import { useDark } from '@vueuse/core'
+import { useDark, useToggle } from '@vueuse/core'
 import { onMounted, ref } from 'vue'
 import axios from 'axios'
 
@@ -43,6 +44,7 @@ interface NasaAPIResponse {
   title: string
   url: string
 }
+const [displayInfos, toggleDisplayInfos] = useToggle()
 const payload = ref<NasaAPIResponse>({
   copyright: '',
   explanation: '',
@@ -54,7 +56,7 @@ onMounted(async () => {
   try {
     const { data } = await axios.get<NasaAPIResponse>('https://api.nasa.gov/planetary/apod', {
       params: {
-        api_key: JSON.parse(import.meta.env.VITE_NASA_API_KEY),
+        api_key: import.meta.env.VITE_NASA_API_KEY,
       },
 
     })
